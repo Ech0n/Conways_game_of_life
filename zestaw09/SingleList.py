b06ae8993a3275a8cc46e1a4cc3a42bcb84b6b7a
Ech0n <matid@spoko.pl>
2022-12-07 00:11:01 +0100

    new file:   SingleList.py 	new file:   test_singlelist.py 	SingleList.py: Dodano funkcje remove_tail, join, clear;

---
+++ zestaw09/SingleList.py
@@ -0,0 +1,111 @@
+class Node:
+    """Klasa reprezentująca węzeł listy jednokierunkowej."""
+
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+    def __eq__(self,other):
+        if not isinstance(other,Node):
+            return False
+        return self.data == other.data
+
+    def __ne__(self, other) -> bool:
+        return not self == other
+
+    def __str__(self):
+        return str(self.data)
+
+class SingleList:
+    """Klasa reprezentująca całą listę jednokierunkową."""
+
+    def __init__(self):
+        self.length = 0   # nie trzeba obliczać za każdym razem
+        self.head :Node = None
+        self.tail :Node = None
+    
+    def __repr__(self):
+        if self.head == None:
+            return "None"
+        return f"{self.head.data}...{self.length}...{self.tail} "
+
+    def __eq__(self,other): #Funkcja pozwala na wykonanie testow
+        if self.length != other.length:
+            return False
+        n = self.head
+        k = other.head
+        while(n!= None):
+            if(n!=k):
+                return False
+            n = n.next
+            k = k.next
+        return True
+    
+    def __ne__(self,other):
+        return not self == other
+
+    def is_empty(self):
+        #return self.length == 0
+        return self.head is None
+
+    def count(self):   # tworzymy interfejs do odczytu
+        return self.length
+
+    def insert_head(self, node):
+        if self.head:   # dajemy na koniec listy
+            node.next = self.head
+            self.head = node
+        else:   # pusta lista
+            self.head = self.tail = node
+        self.length += 1
+
+    def insert_tail(self, node):   # klasy O(1)
+        if self.head:   # dajemy na koniec listy
+            self.tail.next = node
+            self.tail = node
+        else:   # pusta lista
+            self.head = self.tail = node
+        self.length += 1
+
+    def remove_head(self):   # klasy O(1)
+        if self.is_empty():
+            raise ValueError("pusta lista")
+        node = self.head
+        if self.head == self.tail:   # self.length == 1
+            self.head = self.tail = None
+        else:
+            self.head = self.head.next
+        node.next = None   # czyszczenie łącza
+        self.length -= 1
+        return node   # zwracamy usuwany node
+    
+    def remove_tail(self):   # klasy O(n)
+        if self.is_empty():
+            raise ValueError("pusta lista")
+        node = self.tail
+        prev = self.head
+        self.length -= 1
+        if node == prev:
+            self.head = None
+        else:
+            while(prev.next != node):
+                prev = prev.next
+        prev.next = None
+        self.tail = prev
+        return node
+        
+    def join(self, other : "SingleList"):   # klasy O(1)
+        if other.head == None:
+            return
+        else:
+            self.insert_tail(other.remove_head())
+            self.join(other)
+
+
+    def clear(self):   # czyszczenie listy
+        while self.head != None:
+            self.length -= 1
+            t = self.head
+            self.head = self.head.next
+            del t
+        self.tail = None
---
+++ zestaw09/test_singlelist.py
@@ -0,0 +1,94 @@
+from SingleList import SingleList, Node
+import pytest
+
+class TestSingleList:
+    @pytest.fixture
+    def A(self):
+        lista = SingleList()
+        lista.insert_tail(Node(1))
+        lista.insert_tail(Node(2))
+        lista.insert_tail(Node(3))
+        lista.insert_tail(Node(4))
+        return lista
+
+    @pytest.fixture
+    def B(self):
+        lista = SingleList()
+        lista.insert_tail(Node(1))
+        lista.insert_tail(Node(2))
+        return lista
+    @pytest.fixture
+    def C(self):
+        lista = SingleList()
+        lista.insert_tail(Node(1))
+        lista.insert_tail(Node(2))
+        lista.insert_tail(Node(3))
+        return lista
+
+    @pytest.fixture
+    def D(self):
+        lista = SingleList()
+        lista.insert_tail(Node(1))
+        lista.insert_tail(Node(2))
+        lista.insert_tail(Node(3))
+        lista.insert_tail(Node(4))
+        lista.insert_tail(Node(1))
+        lista.insert_tail(Node(2))
+        lista.insert_tail(Node(1))
+        lista.insert_tail(Node(2))
+        lista.insert_tail(Node(3))
+        lista.insert_tail(Node(1))
+        return lista
+
+    @pytest.fixture
+    def S(self):
+        lista = SingleList()
+        lista.insert_tail(Node(1))
+        return lista
+
+    @pytest.fixture
+    def E(self):
+        lista = SingleList()
+        return lista
+
+    def test_remove_tail(self,A,B,C,S,E):
+        usuniete = A.remove_tail()
+        assert usuniete.data == 4
+        assert A == C
+        #Sprawdzam na wypadek jakby porownanie list nie dzialalo prawidlowo
+        assert not A == B 
+        usuniete = B.remove_tail()
+        assert usuniete.data == 2
+        assert B.length == 1
+        assert B.tail.data == 1
+        assert B.head.data == 1
+        assert B == S
+        usuniete = S.remove_tail()
+        assert S.length == 0
+        assert S == E
+        assert usuniete.data == 1
+
+    def test_join(self,A,B,C,D,E,S):
+        len = A.count() + B.count() + C.count() + S.count()
+        A.join(B)
+        assert B == E
+        assert A.count() == 6
+        A.join(C)
+        A.join(S)
+        A.join(E)
+        assert A == D
+        assert A.count() == len
+        assert A.tail.data == 1 and A.head.data  == 1
+        assert C.count() == 0
+    def test_clear(self,S,E,D):
+        D.clear()
+        S.clear()
+        E.clear()
+        assert D.tail == None
+        assert D.head == None
+        assert D.count() == 0
+        assert S.count() == 0
+        assert E.count() == 0        
+
+    if __name__ == "__main__":
+        pytest.main()
