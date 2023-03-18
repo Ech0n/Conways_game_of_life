ec53fcb02fcec9b18683d18cd80a4ce30dad3311
Ech0n <43513980+Ech0n@users.noreply.github.com>
2023-03-18 16:54:17 +0100

    Mirrored project from my private repo

--- zestaw03/zad_3_9.py
+++
@@ -1,6 +0,0 @@
-sekwencja = [[],[4],(1,2),[3,4],(5,6,7)]
-wynik = [sum(x) for x in sekwencja]
-
-print(wynik)
-assert wynik == [0,4,3,7,18]
-
--- zestaw04/zad_4_3.py
+++
@@ -1,8 +0,0 @@
-def factorial(x:int) -> int:
-    ret = 1
-    for i in range(1,x+1):
-        ret*=i
-    return ret
-
-x = int(input("Podaj liczbe: "))
-print(factorial(x))
